fbd1e906f510ca1d612e98ec1774e14627220663
digiconvent <digiconvent1815@gmail.com>
2025-08-24 19:33:53 +0200

    works good enough

--- main.go
+++ main.go
@@ -1,11 +1,1 @@
 package install_on_debian
-
-import (
-	"fmt"
-	"os"
-)
-
-func Main() {
-	fmt.Println("this works")
-	os.Exit(0)
-}
---
+++ service.go
@@ -0,0 +1,73 @@
+package install_on_debian
+
+import (
+	"os"
+	"os/exec"
+)
+
+func servicePath(name string) string {
+	return `/etc/systemd/system/` + name + `.service`
+}
+
+func serviceFileContents(name string) string {
+	return `
+	[Unit]
+	Description=` + name + `
+	After=network.target
+	
+	[Service]
+	Type=simple
+	ExecStart=/home/` + name + `/main --run
+	Restart=always
+	User=` + name + `
+	Group=` + name + `
+	StandardOutput=journal
+	StandardError=journal
+	AmbientCapabilities=CAP_NET_BIND_SERVICE
+	
+	[Install]
+	WantedBy=multi-user.target`
+}
+
+func InstallService(name string) (*installedService, error) {
+	// make sure that a service file is put into place
+	if _, err := os.Stat(servicePath(name)); err != nil {
+		err := os.WriteFile(servicePath(name), []byte(serviceFileContents(name)), 0644)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &installedService{name: name}, nil
+}
+
+type installedService struct {
+	name string
+}
+
+func (i *installedService) Start(name string) (*startedService, error) {
+	for _, command := range []string{
+		"systemctl daemon-reload",
+		"systemctl enable" + name,
+		"systemctl start" + name} {
+		err := exec.Command("sudo", command).Run()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &startedService{name: name}, nil
+}
+func (i *installedService) UninstallService(name string) (*userAccount, error) {
+	err := os.Remove(servicePath(name))
+	if err != nil {
+		return nil, err
+	}
+	return &userAccount{name: name}, nil
+}
+
+type startedService struct {
+	name string
+}
+
+func (s *startedService) StopService() error {
+	return exec.Command("sudo", "systemctl stop"+s.name).Run()
+}
