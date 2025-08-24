24df78def6363be11487cd2c415f23e8000b4c9d
digiconvent <digiconvent1815@gmail.com>
2025-08-24 17:58:18 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DigiConvent/install_on_debian
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package install_on_debian
+
+import (
+	"fmt"
+	"os"
+)
+
+func Main() {
+	fmt.Println("this works")
+	os.Exit(0)
+}
